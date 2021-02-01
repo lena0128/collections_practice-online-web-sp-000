b536c9b44dc0849afe4fbd5c4810c0e86fa6b691
Lena Hou <lenahou2013@gmail.com>
2021-02-01 18:19:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,15 @@ def sort_array_asc(array)
     num1 <=> num2
   end
 end
+
+def sort_array_desc(array)
+  array.sort do |num1, num2|
+    if num1 == num2
+      0
+    elsif num1 > num2
+      -1
+    elsif num1 < num2
+      1
+  end
+end
+        
